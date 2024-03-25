74862dd885af12347d8e484300734e4f285a2087
amir504036 <amir504036@gmail.com>
2024-03-25 07:04:05 +0330

    fix fetch

--- src/jsonApi/factories/ApiDataFactory.ts
+++ src/jsonApi/factories/ApiDataFactory.ts
@@ -1,156 +1,166 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { ApiDataInterface } from "../interfaces/ApiDataInterface";
 import { ApiResponseInterface } from "../interfaces/ApiResponseInterface";
 const dns = require("dns");
 export class ApiDataFactory {
-    public static classMap = new Map<string, { new (): ApiDataInterface }>();
+	public static classMap = new Map<string, { new (): ApiDataInterface }>();
 
-    public static registerObjectClass(key: string, classConstructor: { new (): ApiDataInterface }) {
-        if (!this.classMap.has(key)) this.classMap.set(key, classConstructor);
-    }
+	public static registerObjectClass(key: string, classConstructor: { new (): ApiDataInterface }) {
+		if (!this.classMap.has(key)) this.classMap.set(key, classConstructor);
+	}
 
-    private static async _request<T extends ApiDataInterface>(
-        method: string,
-        classKey: string,
-        params?: any,
-        body?: any
-    ): Promise<ApiResponseInterface> {
-        const factoryClass = this.classMap.get(classKey);
+	private static async _request<T extends ApiDataInterface>(
+		method: string,
+		classKey: string,
+		params?: any,
+		body?: any
+	): Promise<ApiResponseInterface> {
+		const factoryClass = this.classMap.get(classKey);
+
+		if (!factoryClass) {
+			throw new Error(`Class not registered for key: ${classKey}`);
+		}
+
+		const response: ApiResponseInterface = {
+			ok: true,
+			response: 0,
+			data: [],
+			error: "",
+		};
+
+		let link = params?.link;
+		if (!link) link = new factoryClass().generateApiUrl(params);
         dns.setDefaultResultOrder("ipv4first")
-        if (!factoryClass) {
-            throw new Error(`Class not registered for key: ${classKey}`);
-        }
 
-        const response: ApiResponseInterface = {
-            ok: true,
-            response: 0,
-            data: [],
-            error: "",
-        };
+		let token: string | undefined = undefined;
+		if (typeof window === "undefined") {
+			const serverCookies = await import("next/headers");
+			const cookieStore = serverCookies.cookies();
 
-        let link = params?.link;
-        if (!link) link = new factoryClass().generateApiUrl(params);
+			token =
+				cookieStore.get("next-auth.session-token")?.value ??
+				cookieStore.get("__Secure-next-auth.session-token")?.value ??
+				undefined;
+			if (!link.startsWith("http")) link = process.env.NEXT_PUBLIC_API_URL + link;
+		} else {
+			if (link.startsWith("http")) link = link.substring(process.env.NEXT_PUBLIC_API_URL?.length ?? 0);
+			link = process.env.NEXT_PUBLIC_INTERNAL_API_URL + "?uri=" + encodeURIComponent(link);
+		}
 
-        let token: string | undefined = undefined;
-        if (typeof window === "undefined") {
-            const serverCookies = await import("next/headers");
-            const cookieStore = serverCookies.cookies();
+		const options: RequestInit = {
+			method: method,
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: body ? JSON.stringify(body) : undefined,
+		};
 
-            token =
-                cookieStore.get("next-auth.session-token")?.value ??
-                cookieStore.get("__Secure-next-auth.session-token")?.value ??
-                undefined;
-            if (!link.startsWith("http")) link = process.env.NEXT_PUBLIC_API_URL + link;
-        } else {
-            if (link.startsWith("http")) link = link.substring(process.env.NEXT_PUBLIC_API_URL?.length ?? 0);
-            link = process.env.NEXT_PUBLIC_INTERNAL_API_URL + "?uri=" + encodeURIComponent(link);
-        }
+		if (token) {
+			options.headers = {
+				...options.headers,
+				Authorization: `Bearer ${token}`,
+			};
+		}
 
-        const axiosConfig: AxiosRequestConfig = {
-            method: method,
-            url: link,
-            headers: {
-            //     'Accept-Encoding': 'identity',
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            data: body ? JSON.stringify(body) : undefined,
-        };
+		//if (params?.revalidate) options.cache = "reload";
+		// options.cache = "force-cache";
 
-        // if (token) {
-        //     axiosConfig.headers = {
-        //         ...axiosConfig.headers,
-        //         Authorization: `Bearer ${token}`,
-        //     };
-        // }
+		const apiResponse = await fetch(link, options);
 
-        try {
-            const axiosResponse: AxiosResponse = await axios(axiosConfig);
+		response.ok = apiResponse.ok;
+		response.response = apiResponse.status;
 
-            response.ok = axiosResponse.status >= 200 && axiosResponse.status < 300;
-            response.response = axiosResponse.status;
+		if (!apiResponse.ok) {
+			const json = await apiResponse.json();
+			if (json.message !== undefined) {
+				if (Array.isArray(json.message)) {
+					response.error = json.message.join(", ");
+				} else {
+					response.error = json.message;
+				}
+			} else {
+				response.error = apiResponse.statusText;
+			}
+			return response;
+		}
 
-            if (!response.ok) {
-                response.error = axiosResponse.statusText;
-                return response;
-            }
+		if (apiResponse.status === 204) return response;
 
-            if (axiosResponse.status === 204) return response;
+		try {
+			const jsonApi: any = await apiResponse.json();
 
-            const jsonData: any = axiosResponse.data;
+			const included: any = jsonApi.included ?? [];
 
-            const included: any = jsonData.included ?? [];
+			if (jsonApi.links) {
+				response.self = jsonApi.links.self;
 
-            if (jsonData.links) {
-                response.self = jsonData.links.self;
+				if (jsonApi.links.next) {
+					response.next = jsonApi.links.next;
+					response.nextPage = async () => ApiDataFactory.get(classKey, { link: jsonApi.links.next });
+				}
 
-                if (jsonData.links.next) {
-                    response.next = jsonData.links.next;
-                    response.nextPage = async () => ApiDataFactory.get(classKey, { link: jsonData.links.next });
-                }
+				if (jsonApi.links.prev) {
+					response.prev = jsonApi.links.prev;
+					response.prevPage = async () => ApiDataFactory.get(classKey, { link: jsonApi.links.prev });
+				}
+			}
 
-                if (jsonData.links.prev) {
-                    response.prev = jsonData.links.prev;
-                    response.prevPage = async () => ApiDataFactory.get(classKey, { link: jsonData.links.prev });
-                }
-            }
+			if (Array.isArray(jsonApi.data)) {
+				const responseData: T[] = [];
 
-            if (Array.isArray(jsonData.data)) {
-                const responseData: T[] = [];
+				for (const data of jsonApi.data) {
+					const object = new factoryClass();
+					object.rehydrate({ jsonApi: data, included: included });
+					responseData.push(object as T);
+				}
 
-                for (const data of jsonData.data) {
-                    const object = new factoryClass();
-                    object.rehydrate({ jsonApi: data, included: included });
-                    responseData.push(object as T);
-                }
+				response.data = responseData;
+			} else {
+				const responseData = new factoryClass();
+				responseData.rehydrate({ jsonApi: jsonApi.data, included: included });
 
-                response.data = responseData;
-            } else {
-                const responseData = new factoryClass();
-                responseData.rehydrate({ jsonApi: jsonData.data, included: included });
+				response.data = responseData;
+			}
+		} catch (e) {
+			console.error(e);
+		}
 
-                response.data = responseData;
-            }
-        } catch (error) {
-            console.error(error);
-        }
+		return response;
+	}
 
-        return response;
-    }
+	public static async get<T extends ApiDataInterface>(classKey: string, params?: any): Promise<ApiResponseInterface> {
+		return this._request<T>("GET", classKey, params);
+	}
 
-    public static async get<T extends ApiDataInterface>(classKey: string, params?: any): Promise<ApiResponseInterface> {
-        return this._request<T>("GET", classKey, params);
-    }
+	public static async post<T extends ApiDataInterface>(
+		classKey: string,
+		params?: any,
+		body?: any
+	): Promise<ApiResponseInterface> {
+		if (!body) body = {};
+		return this._request<T>("POST", classKey, params, body);
+	}
 
-    public static async post<T extends ApiDataInterface>(
-        classKey: string,
-        params?: any,
-        body?: any
-    ): Promise<ApiResponseInterface> {
-        if (!body) body = {};
-        return this._request<T>("POST", classKey, params, body);
-    }
+	public static async put<T extends ApiDataInterface>(
+		classKey: string,
+		params?: any,
+		body?: any
+	): Promise<ApiResponseInterface> {
+		return this._request<T>("PUT", classKey, params, body);
+	}
 
-    public static async put<T extends ApiDataInterface>(
-        classKey: string,
-        params?: any,
-        body?: any
-    ): Promise<ApiResponseInterface> {
-        return this._request<T>("PUT", classKey, params, body);
-    }
+	public static async patch<T extends ApiDataInterface>(
+		classKey: string,
+		params?: any,
+		body?: any
+	): Promise<ApiResponseInterface> {
+		return this._request<T>("PATCH", classKey, params, body);
+	}
 
-    public static async patch<T extends ApiDataInterface>(
-        classKey: string,
-        params?: any,
-        body?: any
-    ): Promise<ApiResponseInterface> {
-        return this._request<T>("PATCH", classKey, params, body);
-    }
-
-    public static async delete<T extends ApiDataInterface>(
-        classKey: string,
-        params?: any
-    ): Promise<ApiResponseInterface> {
-        return this._request<T>("DELETE", classKey, params);
-    }
+	public static async delete<T extends ApiDataInterface>(
+		classKey: string,
+		params?: any
+	): Promise<ApiResponseInterface> {
+		return this._request<T>("DELETE", classKey, params);
+	}
 }
